093ce854c4f182568a0d8fbfd16275e3d9dbf923
nabettu <tatsuaki.w@gmail.com>
2017-01-28 23:27:32 +0900

    嫁ダッシュボタン

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const DashButton = require("dash-button");
-const MilkCocoa = require('milkcocoa');
+const SlackNode = require('slack-node');
+const slack = new SlackNode();
 
-const milkcocoa = new MilkCocoa('blueiwp41pkk.mlkcca.com');//ここのapp-idは自分のmilkcocoaのid
+const webHookUrL = "SLACK URL";
 
-const ds = milkcocoa.dataStore('messages');
-
+slack.setWebhook(webHookUrL);
 const PHY_ADDR = "34:d2:70:85:64:80"; //dashボタンのアドレス
 
 let button = new DashButton(PHY_ADDR);
 
+const serif = ["メッセージ"];
+const photo = ["写真URL"];
+
 button.addListener(() => {
-  ds.push({content : 'dash!'});
-  console.log("dash!");
+    let text = "送りたい文章";
+
+    slack.webhook({
+        username: "NAME",
+        icon_emoji: "ICON",
+        text: text
+    }, function(err, res) {
+        console.log(err);
+    });
+    console.log("dash!");
 });
